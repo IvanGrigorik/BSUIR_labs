6958a26ba3d72f3e4cee3c2d755c694100649e67
Ivan <givan502@gmail.com>
2021-09-07 20:13:02 +0300

    FUCKING START

---
+++ Lab2_Assignment_and_reviewer/cmake-build-debug/CMakeFiles/Lab2_Assignment_and_reviewer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab2_Assignment_and_reviewer.dir/main.cpp.obj"
+  "Lab2_Assignment_and_reviewer.exe"
+  "Lab2_Assignment_and_reviewer.exe.manifest"
+  "Lab2_Assignment_and_reviewer.pdb"
+  "libLab2_Assignment_and_reviewer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab2_Assignment_and_reviewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab2_Assignment_and_reviewer/cmake-build-debug/CMakeFiles/Lab2_Assignment_and_reviewer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lab2_Assignment_and_reviewer.
+# This may be replaced when dependencies are built.
