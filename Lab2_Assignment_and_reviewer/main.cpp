f3bc73c47ecda209f34e1a50ce31763980882be6
Ivan <givan502@gmail.com>
2021-09-12 20:50:21 +0300

    After best code review

--- Lab2_Assignment_and_reviewer/main.cpp
+++ Lab2_Assignment_and_reviewer/main.cpp
@@ -11,14 +11,18 @@ int main () {
             case 1:
                 add_task(tasks);
                 break;
+
             case 2:
                 show_specific_tasks(tasks);
                 break;
+
             case 3:
                 show_tasks(tasks);
                 break;
+
             case 0:
                 return 0;
+
             default:
                 cout << "Invalid input!";
                 system("cls");
