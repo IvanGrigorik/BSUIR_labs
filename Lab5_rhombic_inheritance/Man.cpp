7cbbda95f941f542ea9c996451cdb9ecf9d6d963
Ivan <givan502@gmail.com>
2021-10-19 20:53:49 +0300

    Finished lab

--- Lab5_rhombic_inheritance/Man.cpp
+++ Lab5_rhombic_inheritance/Man.cpp
@@ -4,27 +4,27 @@
 
 #include "Man.h"
 
-Man::Man (string new_name) {
+Man::Man(string new_name) {
     cout << "Man constructor called" << endl;
-    set_name(std::move(new_name));
+    set_name (std::move (new_name));
 }
 
-Man::~Man () {
+Man::~Man() {
     cout << "Man destructor called";
 }
 
-ostream &operator<< (ostream &os, const Man &man) {
+ostream &operator<<(ostream &os, const Man &man) {
     os << "Name: " << man.name;
     return os;
 }
 
-void Man::set_name (string new_name) {
-    this->name = std::move(new_name);
+void Man::set_name(string new_name) {
+    this->name = std::move (new_name);
 }
 
-void Man::re_name () {
+void Man::re_name() {
     cout << "Enter new name: ";
     string new_name{};
-    new_name = get_string("");
-    set_name(new_name);
+    new_name = get_string ("");
+    set_name (new_name);
 }
