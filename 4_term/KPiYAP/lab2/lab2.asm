b01050e8242e0cbd49e460bbab7f2f7ab460b87f
IvanGrigorik <givan502@gmail.com>
2022-03-15 21:55:18 +0300

    Add string compare

--- 4_term/KPiYAP/lab2/lab2.asm
+++ 4_term/KPiYAP/lab2/lab2.asm
@@ -1,65 +1,45 @@
 	.model small
-	.stack 100h
 	
-	.data
-		
-		;;	TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE 
-		message db "hello", 0Dh, 0Ah, '$'
-		;;	TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE TEST ZONE 
-		
-		
-		
-		max_string_size 		equ 	200
-		cur_string_size 		db 	0
-		
-		enter_stirng_message	db 		"Enter your string: $"
-		enter_word_message		db		"Enter your word: $"
-		
-		output_strin_message 	db		"Your string: $"
-		
-		exCode	 				db 		0
-		
-		end_symbol 				db 		0Dh, 0Ah, '$'
-		string 					db		max_string_size DUP(?)
+	;;--MACROS ZONE---------------------------------------------------------------------------------------------------------------------------------
+	;;##############################################################################################################################################
 	
-	.code
-	
-	Start:
-		mov 	ax, @data
-		mov		ds, ax
-	
-	;;-----------------------------------------------------------------
+	;;--MACROS--------------------------------------------------------------------------------------------------------------------------------------
 	;; Basic string output macros
-	;;-----------------------------------------------------------------
-	;; In: 			dx - output string effective address
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	;; In: 			string_to_output - -//-
 	;; Out: 		output string in console
 	;; Registers: 	ah change
-	;;-----------------------------------------------------------------
-	string_out		macro 	
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	string_out		macro 		string_to_output
 		push 	ax
+		push 	dx
 		
+		lea 	dx, string_to_output
 		mov 	ah, 9
 		int 	21h
 		
+		pop 	dx
 		pop 	ax
 	endm
 	
-	;;-----------------------------------------------------------------
+	;;--MACROS--------------------------------------------------------------------------------------------------------------------------------------
 	;; String input by reading every char
-	;;-----------------------------------------------------------------
-	;;
-	;;
-	;;
-	;;-----------------------------------------------------------------
-	string_in 		macro
-		push cx
-		push si
-		push dx
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	;; In: 			"string_to_input", "max_size", "current_string_size" variables
+	;; Out: 		Filled /\ variables
+	;; Registers: 	ax change
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	string_in 		macro		max_size, current_string_size, string_to_input 
+		local	 ent, end_in
 		
-		xor dx, dx
+		push 	cx
+		push 	si
+		push 	dx
+		
+		xor 	dx, dx
 	
-		mov 	cx, max_string_size - 1
-		lea 	si, string
+		mov 	cx, max_size - 1
+		lea 	si, string_to_input
 		
 		ent: 
 			mov 	ah, 1
@@ -75,7 +55,7 @@
 			loop 	ent
 		
 		end_in:
-			mov 	cur_string_size, dl
+			mov 	current_string_size, dl
 			mov		byte ptr [si], '$'
 			
 		pop dx
@@ -83,24 +63,207 @@
 		pop cx
 	endm
 	
+	;;--MACROS--------------------------------------------------------------------------------------------------------------------------------------
+	;; Remove substring from string
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	;; In: 			string, string size, substring, substring size
+	;; Out: 		reformate string and string size
+	;; Registers: 	???????????
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	remove_substr	macro		string, string_size, substring, substring_size
+		
 	
-		mov 		dx, offset enter_stirng_message
-		string_out
-		string_in
+	endm
+	
+	
+	;;--MACROS--------------------------------------------------------------------------------------------------------------------------------------
+	;; Determine string lenght
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	;; In: 			String
+	;; Out: 		cx - string lenght
+	;; Registers: 	cx, di
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	string_lenght	macro		string
+		local not_end, is_end
+		;zxc$ qweq$
+		
+		push 	si
+		
+		lea 	si, string
+		
+		mov 	cx, 0FFFFh
+		
+		not_end:
+			cmp byte ptr [si], '$'
+			jz is_end
+			
+			inc	si
+		loop not_end
+		
+		is_end:
+		not 	cx
+		
+		pop 	si
+	endm
+	
+	
+	
+	;;--MACROS--------------------------------------------------------------------------------------------------------------------------------------
+	;; Compare strings
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	;; In: 			Two strings
+	;; Out: 		Falgs for jumps
+	;; Registers: 	none
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	compare_strings	macro		string1, string2
+		local testing, not_equal
+
+		push ax
+		push di
+		push si
+
+		string_lenght string1
+		mov ax, cx
+		string_lenght string2
+		cmp ax, cx
+		jne not_equal
+
+		lea		di, string1
+		lea 	si, string2
+
+		repe cmpsb
+		jnz  not_equal
+
+		not_equal:
+
+		pop si
+		pop di
+		pop ax
+	endm
+	
+	;;--MACROS--------------------------------------------------------------------------------------------------------------------------------------
+	;; Find substring in the string
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	;; In: 			???????????
+	;; Out: 		???????????
+	;; Registers: 	???????????
+	;;----------------------------------------------------------------------------------------------------------------------------------------------
+	find_substr 	macro		string, substring
+	local is_end, not_end
+		
+		push ax
+		push cx
+		push bx
+		push di
+		
+		lea 	di, string
+		lea 	si, substring
+		
+		string_lenght 	string
+		mov 	ax, cx
+		string_lenght substring
+		mov 	bx, cx
+		
+		sub 	ax, bx
+		jb 		is_end
+		
+		; Запомнить начало слова
+		; В строке дойти до пробела (если его нету - просто сравнить строку string со строкой substring) или $
+ 		; Запомнить эту позицию
+		; Поставить там $ символ
+		; Вызвать функцию сравнения с начала слова
+		; Если слова равны - выйти и вернуть позицию в каком-либо регистре (предпочтительно si)
+		
+		cycle:
+			mov 
 		
 		
 		
 		
-		mov 		dx, offset end_symbol
-		string_out
-		mov 		dx, offset output_strin_message
-		string_out
-		mov 		dx, offset string
-		string_out
+		pop di
+		pop bx
+		pop cx
+		pop ax
+	
+	
+	endm
+	
+	
+	;;##############################################################################################################################################
+	;;--END OF MACROS AND PROCESSES ZONE------------------------------------------------------------------------------------------------------------
+	
+	.stack 100h
+	
+	;;--CONSTANTS AND VARIABLES ZONE----------------------------------------------------------------------------------------------------------------
+	.data
+		max_string_size 		equ 	200
+		cur_string_size 		db 		0
+		cur_substring_size		db		0
 		
+		enter_stirng_message	db 		"Enter your string: $"
+		enter_word_message		db		"Enter your substring: $"
+		
+		out_string_message 		db		"Your string: $"
+		out_substring_message 	db		"Your substring: $"
+		out_reformat_string		db		"String after reformat: $"
+		
+		;;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+		equals					db		"Equals$"
+		;;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+		
+		ex_code	 				db 		0
+		
+		end_symbol 				db 		0Dh, 0Ah, '$'
+		substring				db 		max_string_size DUP(?)
+		string 					db		max_string_size DUP(?)
+	;;--END OF CONSTANTS AND VARIABLES ZONE---------------------------------------------------------------------------------------------------------
+	
+	
+	;;--MAIN CODE ZONE------------------------------------------------------------------------------------------------------------------------------
+	.code
+	
+	Start:
+		mov 	ax, @data
+		mov		ds, ax
+	
+		mov 	es, ax
+	
+	;;--MAIN PROCESS--------------------------------------------------------------------------------------------------------------------------------
+	;; Remove substring "substring" from string "string"
+	 
+		string_out			enter_stirng_message
+		string_in			max_string_size, cur_string_size, string
+		
+		string_out			enter_word_message
+		string_in			max_string_size, cur_substring_size, substring
+		
+		string_out 			out_string_message
+		string_out			string
+		string_out 			end_symbol
+		
+		string_out			out_substring_message
+		string_out 			substring
+		string_out 			end_symbol
+		
+	;; test
+		
+		compare_strings		string, substring
+		jnz mark
+		
+		string_out			end_symbol
+		string_out 			equals
+		
+		mark:
+		
+	;; test
+		
+	;;--EXIT PROCESS--------------------------------------------------------------------------------------------------------------------------------
+	;; Just terminate programm with exit code "ex_code"
 	Exit: 
 		mov 		ah, 4Ch
-		mov 		al, [exCode]
+		mov 		al, [ex_code]
 		int 		21h
 	
 	end Start
+	
+	;;--END OF MAIN CODE ZONE-----------------------------------------------------------------------------------------------------------------------
