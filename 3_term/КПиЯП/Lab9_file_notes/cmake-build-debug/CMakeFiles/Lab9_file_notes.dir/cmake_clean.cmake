7de440aba61c52b1294ca059c83b8f810e4f5286
IvanGrigorik <givan502@gmail.com>
2022-04-10 01:56:47 +0300

    Finished lab

--- 3_term/КПиЯП/Lab9_file_notes/cmake-build-debug/CMakeFiles/Lab9_file_notes.dir/cmake_clean.cmake
+++ 3_term/КПиЯП/Lab9_file_notes/cmake-build-debug/CMakeFiles/Lab9_file_notes.dir/cmake_clean.cmake
@@ -1,8 +1,12 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Lab9_file_notes.dir/main.cpp.obj"
+  "CMakeFiles/Lab9_file_notes.dir/main.cpp.obj.d"
   "CMakeFiles/Lab9_file_notes.dir/src/Notes.cpp.obj"
+  "CMakeFiles/Lab9_file_notes.dir/src/Notes.cpp.obj.d"
   "CMakeFiles/Lab9_file_notes.dir/src/file_work.cpp.obj"
+  "CMakeFiles/Lab9_file_notes.dir/src/file_work.cpp.obj.d"
   "CMakeFiles/Lab9_file_notes.dir/src/menu_functions.cpp.obj"
+  "CMakeFiles/Lab9_file_notes.dir/src/menu_functions.cpp.obj.d"
   "Lab9_file_notes.exe"
   "Lab9_file_notes.exe.manifest"
   "Lab9_file_notes.pdb"
