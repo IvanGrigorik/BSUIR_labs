8810d07778e1744361029d3f4348c3f61ef77e2f
Ivan <givan502@gmail.com>
2022-09-05 15:02:47 -0400

    Remove redundant code

--- 5_term/TOKS/lab1/writer/main.py
+++ 5_term/TOKS/lab1/writer/main.py
@@ -6,9 +6,6 @@ def main():
     ser = serial.Serial('/dev/tnt0', baudrate=9600, stopbits=serial.STOPBITS_TWO)
     print('Port ready write message')
 
-    baudrate_table = [50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400,
-                      4800, 9600, 19200, 28800, 38400, 57600, 76800, 115200]
-
     while True:
         message = input('>> ')
 
@@ -16,7 +13,7 @@ def main():
         if message == 'ch':
             port_number = input('Enter which port speed you want to change (0/1): ')
             new_baudrate = 0
-            while int(new_baudrate) not in baudrate_table:
+            while int(new_baudrate) not in serial.Serial.BAUDRATES:
                 new_baudrate = input('Enter new port speed: ')
 
             match int(port_number):
