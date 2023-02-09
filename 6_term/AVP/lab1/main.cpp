892dd5ce1ebe0fba40b05fd837fb9de6d3c1c193
Ivan <givan502@gmail.com>
2023-02-09 13:19:52 +0300

    Finished lab

--- 6_term/AVP/lab1/main.cpp
+++ 6_term/AVP/lab1/main.cpp
@@ -53,13 +53,14 @@ SmallMatrix multiply_matrices_avx(SmallMatrix &matrix1, SmallMatrix &matrix2) {
     return small_matrix_result;
 }
 
+__attribute__((target("sse,avx")))
 BigMatrix calculate_avx(BigMatrix &matrix1, BigMatrix &matrix2) {
 
     BigMatrix result_big_matrix(NEW_BIG_MATRIX);
 
     for (int i = 0; i < BIG_MATRIX_SIZE; i++) {
-        for (int j = 0; j < BIG_MATRIX_SIZE; j++) {
-            for (int k = 0; k < BIG_MATRIX_SIZE; k++) {
+        for (int k = 0; k < BIG_MATRIX_SIZE; k++) {
+            for (int j = 0; j < BIG_MATRIX_SIZE; j++) {
                 auto multiplication = multiply_matrices_avx(matrix1[i][k], matrix2[k][j]);
                 sum_small_matrices(result_big_matrix[i][j], multiplication);
             }
@@ -99,7 +100,7 @@ BigMatrix calculate(BigMatrix &matrix1, BigMatrix &matrix2) {
 }
 
 __attribute__((target("avx,sse")))
-SmallMatrix multiply_matrices_manually(SmallMatrix &matrix1, SmallMatrix &matrix2) {
+SmallMatrix multiply_matrices_manually_reverse(SmallMatrix &matrix1, SmallMatrix &matrix2) {
 
     SmallMatrix small_matrix_result(NEW_SMALL_MATRIX);
 
@@ -110,15 +111,15 @@ SmallMatrix multiply_matrices_manually(SmallMatrix &matrix1, SmallMatrix &matrix
 
                 auto va = _mm256_loadu_pd(&matrix1[i][k]);
                 auto vb = _mm256_setr_pd(matrix2[k][j],
-                                        matrix2[k + 1][j],
-                                        matrix2[k + 2][j],
-                                        matrix2[k + 3][j]);
+                                         matrix2[k + 1][j],
+                                         matrix2[k + 2][j],
+                                         matrix2[k + 3][j]);
 
                 auto mul = va * vb;
 
-                auto hi = _mm256_extractf128_pd(mul, 1);
+                auto high = _mm256_extractf128_pd(mul, 1);
                 auto low = _mm256_extractf128_pd(mul, 0);
-                result += hi + low;
+                result += high + low;
             }
 
             auto va = _mm_loadu_pd(&matrix1[i][8]);
@@ -127,6 +128,60 @@ SmallMatrix multiply_matrices_manually(SmallMatrix &matrix1, SmallMatrix &matrix
 
             auto res4 = _mm_hadd_pd(result, result);
 
+            // Get first vector
+            small_matrix_result[i][j] += _mm_cvtsd_f64(res4);
+        }
+    }
+
+    return small_matrix_result;
+}
+
+BigMatrix calculate_manually_reverse(BigMatrix &matrix1, BigMatrix &matrix2) {
+
+    BigMatrix result_big_matrix(NEW_BIG_MATRIX);
+
+    for (int i = 0; i < BIG_MATRIX_SIZE; i++) {
+        for (int j = 0; j < BIG_MATRIX_SIZE; j++) {
+            for (int k = 0; k < BIG_MATRIX_SIZE; k++) {
+                auto multiplication = multiply_matrices_manually_reverse(matrix1[i][k], matrix2[k][j]);
+                sum_small_matrices(result_big_matrix[i][j], multiplication);
+            }
+        }
+    }
+    return result_big_matrix;
+}
+
+
+__attribute__((target("avx,sse")))
+SmallMatrix multiply_matrices_manually(SmallMatrix &matrix1, SmallMatrix &matrix2) {
+
+    SmallMatrix small_matrix_result(NEW_SMALL_MATRIX);
+
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            __m128d result{};
+            for (int k = 0; k < MATRIX_SIZE - 2; k += 4) {
+
+                auto va = _mm256_loadu_pd(&matrix1[i][k]);
+                auto vb = _mm256_set_pd(matrix2[k + 3][j],
+                                         matrix2[k + 2][j],
+                                         matrix2[k + 1][j],
+                                         matrix2[k][j]);
+
+                auto mul = va * vb;
+
+                auto high = _mm256_extractf128_pd(mul, 1);
+                auto low = _mm256_extractf128_pd(mul, 0);
+                result += high + low;
+            }
+
+            auto va = _mm_loadu_pd(&matrix1[i][8]);
+            auto vb = _mm_set_pd(matrix2[9][j], matrix2[8][j]);
+            result += va * vb;
+
+            auto res4 = _mm_hadd_pd(result, result);
+
+            // Get first vector
             small_matrix_result[i][j] += _mm_cvtsd_f64(res4);
         }
     }
@@ -149,6 +204,7 @@ BigMatrix calculate_manually(BigMatrix &matrix1, BigMatrix &matrix2) {
     return result_big_matrix;
 }
 
+
 bool compare(BigMatrix &big_matrix1, BigMatrix &big_matrix2) {
 
     for (int i = 0; i < BIG_MATRIX_SIZE; i++) {
@@ -158,7 +214,7 @@ bool compare(BigMatrix &big_matrix1, BigMatrix &big_matrix2) {
                     auto e1 = big_matrix1[i][j][k][m], e2 = big_matrix2[i][j][k][m];
 
                     if (abs(e1 - e2) > 0.01) {
-                        cout << "Matrix not equal!" << e1 << '-' << e2 << ' Index:' << i << j << k << m << endl;
+                        cout << "Matrix not equal!" << e1 << '-' << e2 << " Index:" << i << j << k << m << endl;
                         return false;
                     }
                 }
@@ -181,23 +237,30 @@ int main() {
     randomize_one_matrix(big_matrix2);
 
     auto start = steady_clock::now();
-    auto big_result_avx = calculate_avx(big_matrix1, big_matrix2);
+//    auto big_result_avx = calculate_avx(big_matrix1, big_matrix2);
     auto time = duration_cast<milliseconds>(steady_clock::now() - start).count();
-    cout << "With AVX took me " << time << " milliseconds." << endl;
 
+
+//    cout << "With AVX took me " << time << " milliseconds." << endl;
+//
+//
+//    start = steady_clock::now();
+//    auto big_result = calculate(big_matrix1, big_matrix2);
+//    time = duration_cast<milliseconds>(steady_clock::now() - start).count();
+//    cout << "Without AVX took me " << time << " milliseconds." << endl;
+//    compare(big_result, big_result_avx);
 
     start = steady_clock::now();
-    auto big_result = calculate(big_matrix1, big_matrix2);
-    time = duration_cast<milliseconds>(steady_clock::now() - start).count();
-    cout << "Without AVX took me " << time << " milliseconds." << endl;
-    compare(big_result, big_result_avx);
-
-
-    start = steady_clock::now();
-    auto big_result_manually = calculate_manually(big_matrix1, big_matrix2);
+    auto big_result_manually = calculate_manually_reverse(big_matrix1, big_matrix2);
     time = duration_cast<milliseconds>(steady_clock::now() - start).count();
     cout << "With manually AVX took me " << time << " milliseconds." << endl;
-    compare(big_result_avx, big_result_manually);
+
+
+    start = steady_clock::now();
+    auto big_result_manually_reverse = calculate_manually_reverse(big_matrix1, big_matrix2);
+    time = duration_cast<milliseconds>(steady_clock::now() - start).count();
+    cout << "With manually reversed AVX took me " << time << " milliseconds." << endl;
+    compare(big_result_manually, big_result_manually_reverse);
 
     return 0;
 }
