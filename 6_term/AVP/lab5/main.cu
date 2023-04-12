805d353b124208da8f21f7130381865bbf65f29f
sifi <givan502@gmail.com>
2023-04-12 21:16:47 +0300

    Fix size issue

--- 6_term/AVP/lab5/main.cu
+++ 6_term/AVP/lab5/main.cu
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
-#include <tuple>
 #include <algorithm>
+#include <iomanip>
 #include "Image.h"
 
 #define degToRad(val) (val * M_PI / 180)
@@ -20,8 +20,16 @@ void houghTransform(const Image &image) {
         houghAccum[i].resize(thetasCount);
     }
 
+    const float step = 180.f / static_cast<float>(thetasCount);
+    std::vector<float> thetas;
+    thetas.resize(thetasCount);
 
-    const int step = 180 / thetasCount >= 0 ? 1 : 180 / thetasCount;
+    double ang = -90;
+    while (ang < 90) {
+        ang += step;
+        thetas.push_back(degToRad(ang));
+    }
+
     for (int x = 0; x < image.getWidth(); x++) {
         for (int y = 0; y < image.getHeight(); y++) {
             // Find pixel fits the marker
@@ -29,30 +37,23 @@ void houghTransform(const Image &image) {
                 continue;
             }
 
-            // Create all possible variants of thetas
 
-            // Iterate through all possible lines
-            for (int k = -90, h = 0; k < 90; k += step, h++) {
-                double idx = maxDist + x * cos(degToRad(k)) + y * sin(degToRad(k));
-                houghAccum[static_cast<int>(idx)][h]++;
+
+            // Iterate through all possible lines (need to rewrite)
+            for (double k = -90, h = 0; k < 90; k += step, h++) {
+                double idx = x * cos(degToRad(k)) + y * sin(degToRad(k));
+                houghAccum[static_cast<int>(idx) + maxDist][static_cast<int>(h)]++;
             }
         }
     }
+
+
 }
 
-std::tuple<int, int> findMax() {
-
-    
-    for (int i = 0; i < houghAccum.size(); i++){
-
-    }
-
-    return std::make_tuple<>(1, 2);
-}
 
 int main() {
     using namespace std;
-    Image image{"../images/straight_line.png"};
+    Image image{"../images/line3x3.png"};
     cout << "Image height: " << image.getHeight() << endl << "Image width: " << image.getWidth() << endl;
 
     houghTransform(image);
@@ -61,7 +62,7 @@ int main() {
 
     for (auto &i: houghAccum) {
         for (auto j: i) {
-            cout << j << ' ';
+            cout << setw(2) << j << ' ';
         }
         cout << endl;
     }
