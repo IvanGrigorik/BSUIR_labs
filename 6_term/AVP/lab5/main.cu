86a7109bd32fc091689927e89a3b34f7ec7c7540
sifi <givan502@gmail.com>
2023-04-14 17:43:00 +0300

    Finish line angle detection

--- 6_term/AVP/lab5/main.cu
+++ 6_term/AVP/lab5/main.cu
@@ -5,11 +5,22 @@
 #include "Image.h"
 
 #define degToRad(val) (val * M_PI / 180)
-#define radToDeg(val) (val * 180 / M_PI)
 
 std::vector<std::vector<int>> houghAccum;
 Pixel marker{0, 0, 0};
 
+/// Returns line angle relative to the horizon (see explanation ahead the function)
+/*
+ * |                     ----
+ * |                 ----
+ * |             ---- A (equivalent)
+ * | ****************************** horizon line
+ * |       A ----
+ * |     ----
+ * | ----
+ * |||||||||||||||||||||||||||||||| image x-axis
+ * Return angle A
+ */
 int houghTransform(const Image &image) {
     const int maxDist = static_cast<int>(round(sqrt(pow(image.getHeight(), 2) + pow(image.getWidth(), 2))));
     // Little theta and step optimization
@@ -24,39 +35,38 @@ int houghTransform(const Image &image) {
     const float step = 180.f / static_cast<float>(thetasCount);
     std::vector<float> thetas;
 
-    double angelBotBorder = -90;
-    while (angelBotBorder < 90) {
-        angelBotBorder += step;
-        thetas.push_back(degToRad(angelBotBorder));
-    }
 
-    for (int x = 0; x < image.getWidth(); x++) {
-        for (int y = 0; y < image.getHeight(); y++) {
+    for (int x = 0; x < image.getHeight(); x++) {
+        for (int y = 0; y < image.getWidth(); y++) {
             // Find pixel fits the marker
             if (image.getPixel(x, y) != marker) {
                 continue;
             }
 
             // Iterate through all possible lines (need to rewrite)
-            for (int h = 0; h < thetasCount; h++) {
-                double idx = maxDist + x * cos(thetas[h]) + y * sin(thetas[h]);
-                houghAccum[static_cast<int>(idx)][static_cast<int>(h)]++;
+            double ang = -90;
+            for (int h = 0; h < thetasCount; ang += step, h++) {
+                int idx = static_cast <int>(maxDist + x * cos(degToRad(ang)) + y * sin(degToRad(ang)));
+                houghAccum[idx][h]++;
             }
         }
     }
 
     ulong idx{}, max{};
-    for (int i = 0; i < houghAccum.size(); i++) {
-        for (int j = 0; j < houghAccum[i].size(); j++) {
-            if (houghAccum[i][j] > max) {
-                max = houghAccum[i][j];
-                idx = i * houghAccum.size() + j;
+    for (auto &i: houghAccum) {
+        for (int j = 0; j < i.size(); j++) {
+            if (i[j] > max) {
+                max = i[j];
+                idx = j;
             }
         }
     }
 
-    auto angle = thetas[static_cast<int>(idx % thetasCount)];
-    return radToDeg(angle);
+    double angle{-90};
+    for (int i = 0; i < idx; i++) {
+        angle += step;
+    }
+    return static_cast<int>(angle);
 }
 
 
@@ -65,16 +75,12 @@ int main() {
     Image image{"../images/line65.png"};
     cout << "Image height: " << image.getHeight() << endl << "Image width: " << image.getWidth() << endl;
 
+    //
     auto angle = houghTransform(image);
 
-    cout << endl << "Hough result: " << angle << endl;
 
-//    for (auto &i: houghAccum) {
-//        for (auto j: i) {
-//            cout << setw(2) << j << ' ';
-//        }
-//        cout << endl;
-//    }
+
+    cout << endl << "Hough result: " << angle << endl;
 
     return 0;
 }
