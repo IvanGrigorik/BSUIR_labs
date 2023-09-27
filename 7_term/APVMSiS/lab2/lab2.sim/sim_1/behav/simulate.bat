605be327c9dc401f2b4be94bb1efc0b24d7f4708
Ivan <73799907+IvanGrigorik@users.noreply.github.com>
2023-09-27 20:53:05 +0300

    Delete 7_term/APVMSiS/lab2/lab2.sim/sim_1/behav directory

--- 7_term/APVMSiS/lab2/lab2.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=E:\\utils\\Vivado\\Vivado\\2017.2\\bin
-call %xv_path%/xsim SHFT_REG_tb_behav -key {Behavioral:sim_1:Functional:SHFT_REG_tb} -tclbatch SHFT_REG_tb.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
