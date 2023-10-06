b83556caa63cbed3682c0a6cd965414b614f200d
sifi <givan502@gmail.com>
2023-10-06 22:14:41 +0300

    Finished lab

--- 7_term/Mod/lab1/src/main.rs
+++ 7_term/Mod/lab1/src/main.rs
@@ -28,11 +28,18 @@ enum TabsStates {
 }
 
 struct MyApp {
+    // For RNG's
     a_buffer: u64,
     x1_buffer: u64,
     m_buffer: u64,
     random_numbers: Vec<u64>,
     random_numbers_bar: Vec<plot::Bar>,
+    r0_buffer: u64,
+    r1_buffer: u64,
+    shift_buffer: u64,
+    count: usize,
+
+    // For tests
     test_state: TestStates,
     tab_state: TabsStates,
 
@@ -58,10 +65,14 @@ impl Default for MyApp {
             m_buffer: (2_147_483_647),
             random_numbers: Vec::new(),
             random_numbers_bar: Vec::new(),
+            r0_buffer: 121241445515252,
+            r1_buffer: 31231455523,
+            shift_buffer: 248432131643609289,
+            count: 100_000,
 
             // Ui states
             test_state: TestStates::Nothing,
-            tab_state: TabsStates::MiddleMethod,
+            tab_state: TabsStates::Lehmer,
 
             // Tests results and AVG value of whole generated vector
             average: 0,
@@ -80,7 +91,16 @@ impl Default for MyApp {
 }
 
 impl MyApp {
-    fn integer_edit_field(ui: &mut egui::Ui, value: &mut u64) -> egui::Response {
+    fn u64_edit_field(ui: &mut egui::Ui, value: &mut u64) -> egui::Response {
+        let mut tmp_value = format!("{}", value);
+        let res = ui.text_edit_singleline(&mut tmp_value);
+        if let Ok(result) = tmp_value.parse() {
+            *value = result;
+        }
+        res
+    }
+
+    fn usize_edit_field(ui: &mut egui::Ui, value: &mut usize) -> egui::Response {
         let mut tmp_value = format!("{}", value);
         let res = ui.text_edit_singleline(&mut tmp_value);
         if let Ok(result) = tmp_value.parse() {
@@ -101,20 +121,53 @@ impl MyApp {
         }
     }
 
+    // RNG's
     fn generate_lehmer(&mut self) {
         let a = self.a_buffer as u128;
         let x1 = self.x1_buffer as u128;
         let m = self.m_buffer as u128;
         self.random_numbers.push(x1 as u64);
 
-        for i in 1..100_000 {
+        for i in 1..self.count {
             let t = a * self.random_numbers[i - 1] as u128;
             let _tmp = t % m;
             self.random_numbers.push(_tmp as u64);
         }
     }
 
-    fn generate_middle(&mut self) {}
+    fn generate_middle(&mut self) {
+        let mut r0 = self.r0_buffer as u128;
+        let mut r1 = self.r1_buffer as u128;
+
+        let mask: u128 = 0xFFFFFFFFFFFFFFFF00000000; // mask to identify middle part of the number
+        for _ in 0..self.count {
+            let mut new_r = r0 * r1;
+
+            new_r &= mask;
+            new_r >>= 32;
+
+            self.random_numbers.push(new_r as u64);
+
+            r0 = r1;
+            r1 = new_r;
+        }
+    }
+
+    fn generate_shift(&mut self) {
+        let mut num = self.shift_buffer;
+        for _ in 0..self.count {
+            // Identify two last bits bit
+            let b_0 = num & 1;
+            let b_1 = (num & 2) >> 1;
+            // Generate new last bit
+            let n_bit = (b_0 ^ b_1) << 63;
+
+            // Shift our number and put the last bit
+            num >>= 1;
+            num |= n_bit;
+            self.random_numbers.push(num);
+        }
+    }
 
     // Function that transform random vector to tested
     fn initialize_tested_vector(&mut self) {
@@ -134,6 +187,7 @@ impl MyApp {
     }
 
     // Nist tests
+
     fn freq_test(&self, to_test: &Vec<bool>) -> String {
         let str: String = to_test.iter().map(|x| (*x as u8).to_string()).collect();
         let data = BitsData::from_text(str);
@@ -210,38 +264,49 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ctx.set_pixels_per_point(1.5);
-            Grid::new("tab_grid").min_col_width(20.).max_col_width(100.).show(ui, |ui| {
-                if ui.button("Lehmer").clicked() {
-                    self.tab_state = TabsStates::Lehmer;
-                } else if ui.button("Middle").clicked() {
-                    self.tab_state = TabsStates::MiddleMethod;
-                }
-            });
 
+            // Pseudo-tabs
+            Grid::new("tab_grid")
+                .min_col_width(20.)
+                .max_col_width(100.)
+                .show(ui, |ui| {
+                    if ui.button("Lehmer").clicked() {
+                        self.tab_state = TabsStates::Lehmer;
+                    } else if ui.button("Middle").clicked() {
+                        self.tab_state = TabsStates::MiddleMethod;
+                    } else if ui.button("LFSR").clicked() {
+                        self.tab_state = TabsStates::ShiftReg;
+                    }
+                    ui.end_row();
+                    let sample_size = ui.label("Sample size: ");
+                    MyApp::usize_edit_field(ui, &mut self.count).labelled_by(sample_size.id);
+                });
+
+            // Tabs internal logic
             match self.tab_state {
                 TabsStates::Lehmer => {
                     ui.heading("Lehmer generator");
 
                     // Input grid
-                    Grid::new("input_grid")
+                    Grid::new("lehmer_input_grid")
                         .min_col_width(20.0)
                         .max_col_width(180.0)
                         .striped(true)
                         .show(ui, |ui| {
                             let a_label = ui.label("a: ");
-                            MyApp::integer_edit_field(ui, &mut self.a_buffer)
+                            MyApp::u64_edit_field(ui, &mut self.a_buffer)
                                 .labelled_by(a_label.id);
                             ui.label(format!("a: {}", self.a_buffer));
                             ui.end_row();
 
                             let x1_label = ui.label("X1: ");
-                            MyApp::integer_edit_field(ui, &mut self.x1_buffer)
+                            MyApp::u64_edit_field(ui, &mut self.x1_buffer)
                                 .labelled_by(x1_label.id);
                             ui.label(format!("X1: {}", self.x1_buffer));
                             ui.end_row();
 
                             let m_label = ui.label("Mod m: ");
-                            MyApp::integer_edit_field(ui, &mut self.m_buffer)
+                            MyApp::u64_edit_field(ui, &mut self.m_buffer)
                                 .labelled_by(m_label.id);
                             ui.label(format!("M: {}", self.m_buffer));
                             ui.end_row();
@@ -253,69 +318,115 @@ impl eframe::App for MyApp {
                         self.refresh_numbers();
                         self.test_state = TestStates::ShowChart;
                     }
+                }
 
-                    if ui.button("Test!").clicked() {
-                        self.initialize_tested_vector();
-                        self.freq_result = self.freq_test(&self.tested_vec);
-                        self.block_freq_result = self.block_freq_test(&self.tested_vec);
-                        self.cusum_result = self.cusum_test(&self.tested_vec);
-                        self.run_ones_result = self.run_ones_test(&self.tested_vec);
-                        self.run_result = self.run_test(&self.tested_vec);
-                        self.rank_result = self.rank_test(&self.tested_vec);
-                        self.fft_result = self.fft_test(&self.tested_vec);
-                        self.non_over_result = self.non_over_test(&self.tested_vec);
-                        self.over_result = self.over_test(&self.tested_vec);
+                TabsStates::MiddleMethod => {
+                    ui.heading("Middle multiply method generator");
 
-                        self.test_state = TestStates::ShowTestResults;
-                    }
+                    Grid::new("middle_input_grid")
+                        .min_col_width(20.0)
+                        .max_col_width(180.0)
+                        .striped(true)
+                        .show(ui, |ui| {
+                            let r0_buffer = ui.label("R0: ");
+                            MyApp::u64_edit_field(ui, &mut self.r0_buffer)
+                                .labelled_by(r0_buffer.id);
+                            ui.label(format!("R0: {}", self.r0_buffer));
+                            ui.end_row();
 
-                    match self.test_state {
-                        TestStates::ShowChart => {
-                            let plot = plot::Plot::new("Histogram: ");
-                            plot.show(ui, |plot_ui| {
-                                plot_ui.bar_chart(BarChart::new(self.random_numbers_bar.clone()))
-                            });
-                        }
+                            let r1_buffer = ui.label("R1: ");
+                            MyApp::u64_edit_field(ui, &mut self.r1_buffer)
+                                .labelled_by(r1_buffer.id);
+                            ui.label(format!("R1: {}", self.r1_buffer));
+                            ui.end_row();
+                        });
 
-                        TestStates::ShowTestResults => {
-                            Grid::new("tests")
-                                .min_col_width(10.)
-                                .max_col_width(500.)
-                                .striped(true)
-                                .show(ui, |ui| {
-                                    ui.set_width(100.);
-
-                                    ui.label(format!("Frequency test: {}", self.freq_result));
-                                    ui.end_row();
-                                    ui.label(format!("Cumulative sum test: {}", self.cusum_result));
-                                    ui.end_row();
-                                    ui.label(format!("Run test: {}", self.run_result));
-                                    ui.end_row();
-                                    ui.label(format!("Run ones test: {}", self.run_ones_result));
-                                    ui.end_row();
-                                    ui.label(format!("Matrix rank test: {}", self.rank_result));
-                                    ui.end_row();
-                                    ui.label(format!("FFT test: {}", self.fft_result));
-                                    ui.end_row();
-                                    ui.label(format!(
-                                        "Overlapping template test: {}",
-                                        self.over_result
-                                    ));
-                                    ui.end_row();
-                                    ui.label(format!(
-                                        "Non overlapping template test: {}",
-                                        self.non_over_result
-                                    ));
-                                });
-                        }
-
-                        _ => (),
+                    if ui.button("Generate!").clicked() {
+                        self.clear_numbers();
+                        self.generate_middle();
+                        self.refresh_numbers();
+                        self.test_state = TestStates::ShowChart;
                     }
                 }
 
-                TabsStates::MiddleMethod => {}
+                TabsStates::ShiftReg => {
+                    ui.heading("LFSR-based generator");
 
-                TabsStates::ShiftReg => {}
+                    Grid::new("lfsr_input_grid")
+                        .min_col_width(20.0)
+                        .max_col_width(180.0)
+                        .striped(true)
+                        .show(ui, |ui| {
+                            let first_num = ui.label("First number: ");
+                            MyApp::u64_edit_field(ui, &mut self.shift_buffer)
+                                .labelled_by(first_num.id);
+
+                            ui.label(format!("Init number: {}", self.shift_buffer));
+                            ui.end_row();
+                        });
+
+                    if ui.button("Generate!").clicked() {
+                        self.clear_numbers();
+                        self.generate_shift();
+                        self.refresh_numbers();
+                        self.test_state = TestStates::ShowChart;
+                    }
+                }
+            }
+
+            if ui.button("Test!").clicked() {
+                self.initialize_tested_vector();
+                self.freq_result = self.freq_test(&self.tested_vec);
+                self.block_freq_result = self.block_freq_test(&self.tested_vec);
+                self.cusum_result = self.cusum_test(&self.tested_vec);
+                self.run_ones_result = self.run_ones_test(&self.tested_vec);
+                self.run_result = self.run_test(&self.tested_vec);
+                self.rank_result = self.rank_test(&self.tested_vec);
+                self.fft_result = self.fft_test(&self.tested_vec);
+                self.non_over_result = self.non_over_test(&self.tested_vec);
+                self.over_result = self.over_test(&self.tested_vec);
+
+                self.test_state = TestStates::ShowTestResults;
+            }
+
+            match self.test_state {
+                TestStates::ShowChart => {
+                    let plot = plot::Plot::new("Histogram: ");
+                    plot.show(ui, |plot_ui| {
+                        plot_ui.bar_chart(BarChart::new(self.random_numbers_bar.clone()))
+                    });
+                }
+
+                TestStates::ShowTestResults => {
+                    Grid::new("tests")
+                        .min_col_width(10.)
+                        .max_col_width(500.)
+                        .striped(true)
+                        .show(ui, |ui| {
+                            ui.set_width(100.);
+
+                            ui.label(format!("Frequency test: {}", self.freq_result));
+                            ui.end_row();
+                            ui.label(format!("Cumulative sum test: {}", self.cusum_result));
+                            ui.end_row();
+                            ui.label(format!("Run test: {}", self.run_result));
+                            ui.end_row();
+                            ui.label(format!("Run ones test: {}", self.run_ones_result));
+                            ui.end_row();
+                            ui.label(format!("Matrix rank test: {}", self.rank_result));
+                            ui.end_row();
+                            ui.label(format!("FFT test: {}", self.fft_result));
+                            ui.end_row();
+                            ui.label(format!("Overlapping template test: {}", self.over_result));
+                            ui.end_row();
+                            ui.label(format!(
+                                "Non overlapping template test: {}",
+                                self.non_over_result
+                            ));
+                        });
+                }
+
+                _ => (),
             }
         });
     }
