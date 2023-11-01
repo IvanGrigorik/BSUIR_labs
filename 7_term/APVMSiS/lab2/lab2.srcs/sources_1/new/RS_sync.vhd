e11c188056644569098a75b66f5caa5e7c084431
Ivan <givan502@gmail.com>
2023-11-01 18:00:42 +0300

    Add tests

--- 7_term/APVMSiS/lab2/lab2.srcs/sources_1/new/RS_sync.vhd
+++ 7_term/APVMSiS/lab2/lab2.srcs/sources_1/new/RS_sync.vhd
@@ -3,29 +3,29 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity RS_sync is
   port ( 
-        C : in std_logic;
-        S : in std_logic;
-        R : in std_logic;
-        D : in std_logic ;
-        Q : out std_logic := '0';
-        nQ : out std_logic := '1');
+        C : in std_logic    := '0';
+        S : in std_logic    := '0';
+        R : in std_logic    := '0';
+        D : in std_logic    := '0';
+        Q : out std_logic   := '0';
+        nQ : out std_logic  := '1');
 
 end RS_sync;
 
 architecture Behavioral of RS_sync is
 begin
-    
     process (C, S, R, D) is 
     begin
-    
-        if (R = '1') then
-            Q <= '0';
-            nQ <= '1';
-        elsif (C'event and C = '1') then
-            if (S = '1') then
-                Q <= '1';
-                nQ <= '0';
-            else 
+    -- ???? ?????? ???????? ? R ? S. ????????? (? ?? ??????? ?????? ??? ??????????)
+
+        if (C'event and C = '1') then
+            if (R = '1') then
+                Q <= '0';
+                nQ  <= '1';    
+            elsif (S = '1') then
+                Q   <= '1';
+                nQ  <= '0';
+            else
                 Q <= D;
                 nQ <= not D;
             end if;
