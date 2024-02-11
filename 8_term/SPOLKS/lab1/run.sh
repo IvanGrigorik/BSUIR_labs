4f0e5bebcc956a514c38881aff082193b3dee5cf
sifi <givan502@gmail.com>
2024-02-11 13:57:04 +0300

    Remove prov version

--- 8_term/SPOLKS/lab1/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-h_str='How to use:
-		./run.sh X, where X - threads number'
-mpic++ -std=c++20 Matrix.cpp Matrix.hpp matrix_test.cpp  -o out.exe
-[[ -z ${1} ]] && echo ${h_str} || mpirun -n ${1} ./out.exe
